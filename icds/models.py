27c535fc0e2335c071ffc02ffe3a8e5d829bd3d5
Jordan Dimov <32362173+jordan-dimov@users.noreply.github.com>
2024-04-06 20:41:40 +0100

    Adds SQLModel DB schema and implemented build-index command using SQLite (#10)

---
+++ icds/models.py
@@ -0,0 +1,43 @@
+from sqlmodel import Field, SQLModel, create_engine, Session, select
+from datetime import datetime
+
+from icds.settings import settings
+
+
+class DbRepo(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    name: str
+    path: str | None = None
+    remote_url: str | None = None
+    description: str | None = None
+
+
+class RepoCommit(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    repo_id: int = Field(foreign_key="dbrepo.id")
+    hash: str
+    datetime: datetime
+    author: str
+    commit_message: str
+    file_stats: str | None = None
+    summary: str
+    details: str | None = None
+
+
+engine = create_engine(settings.DATABASE_URL)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_db():
+    with Session(engine) as session:
+        yield session
+
+
+def get_repo_by_name(name: str):
+    with Session(engine) as session:
+        statement = select(DbRepo).where(DbRepo.name == name)
+        repo = session.exec(statement).first()
+        return repo
